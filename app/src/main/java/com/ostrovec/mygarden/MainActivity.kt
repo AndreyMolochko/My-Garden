a2860a712bff77e23a9ee5c2b2c8adc796933f2e
Andrey Molochko <andrey.molochko@exposit.com>
2019-06-02 21:10:26 +0300

    add styles, add dimens, fonts, finish welcom page

--- app/src/main/java/com/ostrovec/mygarden/MainActivity.kt
+++ app/src/main/java/com/ostrovec/mygarden/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.ostrovec.mygarden
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var buttonSignIn: TextView
+    private lateinit var buttonSignUp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.fragment_welcome_page)
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() {
+        buttonSignIn = findViewById(R.id.welcome_page_sign_in)
+        buttonSignUp = findViewById(R.id.welcome_page_sign_up)
+    }
+
+    private fun initListeners(){
+        buttonSignIn.setOnClickListener{
+            println("click on the sign in button")
+        }
+
+        buttonSignUp.setOnClickListener {
+            println("click on the sign up button")
+        }
     }
 }
