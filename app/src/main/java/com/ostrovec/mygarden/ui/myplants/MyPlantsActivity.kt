b3855b5c7f1437ffa9ddfe4eb1bebdd0bca627de
Andrey Molochko <andrey.molochko@exposit.com>
2019-07-25 13:10:51 +0300

    The navigation was added to myPlantsScreen

--- app/src/main/java/com/ostrovec/mygarden/ui/myplants/MyPlantsActivity.kt
+++ app/src/main/java/com/ostrovec/mygarden/ui/myplants/MyPlantsActivity.kt
@@ -1,5 +1,7 @@
 package com.ostrovec.mygarden.ui.myplants
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.ostrovec.mygarden.R
@@ -7,6 +9,13 @@ import com.ostrovec.mygarden.databinding.ActivityMyPlantsBinding
 import com.ostrovec.mygarden.ui.base.BaseActivity
 
 class MyPlantsActivity : BaseActivity() {
+
+    companion object{
+        fun open(context: Context){
+            val intent = Intent(context,MyPlantsActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
 
     private lateinit var binding: ActivityMyPlantsBinding
 
