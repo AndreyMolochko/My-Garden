04356ed9414cfd17d29cb2e2e63658432f73e5d4
Andrey Molochko <andrey.molochko@exposit.com>
2019-08-06 16:14:27 +0300

    create layout alert dialog

--- app/src/main/java/com/ostrovec/mygarden/room/model/Plant.kt
+++ app/src/main/java/com/ostrovec/mygarden/room/model/Plant.kt
@@ -2,6 +2,8 @@ package com.ostrovec.mygarden.room.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -33,7 +35,8 @@ data class Plant(
 
         @ColumnInfo(name = "end_irrigation")
         @SerializedName("end_irrigation")
-        var endIrrigation: Long) : Parcelable {
+        var endIrrigation: Long) : BaseObservable(), Parcelable {
+
     constructor(parcel: Parcel) : this(
             parcel.readInt(),
             parcel.readString(),
