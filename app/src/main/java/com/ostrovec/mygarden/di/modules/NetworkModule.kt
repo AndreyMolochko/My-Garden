ccddfea1a86adc9ad95e49fdd8a1e168858d39eb
Andrey Molochko <andrey.molochko@exposit.com>
2019-08-06 11:20:13 +0300

    fixing bugs (add provides repository in di)

--- app/src/main/java/com/ostrovec/mygarden/di/modules/NetworkModule.kt
+++ app/src/main/java/com/ostrovec/mygarden/di/modules/NetworkModule.kt
@@ -1,6 +1,9 @@
 package com.ostrovec.mygarden.di.modules
 
 import com.google.gson.Gson
+import com.ostrovec.mygarden.repositories.PlantRepository
+import com.ostrovec.mygarden.repositories.PlantRepositoryImp
+import com.ostrovec.mygarden.room.database.AppDatabase
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -12,5 +15,10 @@ class NetworkModule {
     @Singleton
     fun provideGson(): Gson {
         return Gson()
+    }
+
+    @Provides
+    fun providePlantRepository(appDatabase: AppDatabase):PlantRepository{
+        return PlantRepositoryImp(appDatabase)
     }
 }
