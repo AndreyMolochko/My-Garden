d820e38519ad13f6a236d7cb12824313c3ced619
Andrey Molochko <andrey.molochko@exposit.com>
2019-09-02 10:24:17 +0300

    create signInActivity, add in DI module

---
+++ app/src/main/java/com/ostrovec/mygarden/ui/sign/signin/SignInActivity.kt
@@ -0,0 +1,25 @@
+package com.ostrovec.mygarden.ui.sign.signin
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.ostrovec.mygarden.R
+import com.ostrovec.mygarden.databinding.ActivitySignInBinding
+import com.ostrovec.mygarden.ui.base.BaseNavigationActivity
+
+class SignInActivity : BaseNavigationActivity() {
+
+    companion object {
+        fun open(context: Context) {
+            context.startActivity(Intent(context, SignInActivity::class.java))
+        }
+    }
+
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = setContainerView(R.layout.activity_sign_in)
+    }
+}
