2de4e8a93d44a7492a1b4df2fa679ffc7614714e
Andrey Molochko <andrey.molochko@exposit.com>
2019-09-12 16:47:55 +0300

    create item recycler for titile

--- app/src/main/java/com/ostrovec/mygarden/ui/settings/SettingsAdapter.kt
+++ app/src/main/java/com/ostrovec/mygarden/ui/settings/SettingsAdapter.kt
@@ -1,4 +1,32 @@
 package com.ostrovec.mygarden.ui.settings
 
-class SettingsAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ostrovec.mygarden.ui.settings.model.ListItem
+
+class SettingsAdapter(var settingsList: List<ListItem>) : RecyclerView
+.Adapter<RecyclerView.ViewHolder>() {
+
+    private val TYPE_HEADER = 0
+    private val TYPE_LANGUAGE_ITEM = 1
+    private val TYPE_SWITCH_ITEM = 2
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        return settingsList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        //val item = settingsList[position]
+        return -1
+    }
+
+
 }
