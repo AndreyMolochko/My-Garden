7d82f02f33dc69828f27b9032e688f628bb16383
Andrey Molochko <andrey.molochko@exposit.com>
2019-09-12 16:57:50 +0300

    create view holder for headerItem, link layout with model

--- app/src/main/java/com/ostrovec/mygarden/ui/settings/SettingsAdapter.kt
+++ app/src/main/java/com/ostrovec/mygarden/ui/settings/SettingsAdapter.kt
@@ -2,12 +2,14 @@ package com.ostrovec.mygarden.ui.settings
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.ostrovec.mygarden.databinding.ItemRecyclerSettingsTitleBinding
 import com.ostrovec.mygarden.ui.settings.model.ListItem
+import com.ostrovec.mygarden.ui.settings.model.TitleItem
 
 class SettingsAdapter(var settingsList: List<ListItem>) : RecyclerView
 .Adapter<RecyclerView.ViewHolder>() {
 
-    private val TYPE_HEADER = 0
+    private val TYPE_HEADER_ITEM = 0
     private val TYPE_LANGUAGE_ITEM = 1
     private val TYPE_SWITCH_ITEM = 2
 
@@ -26,6 +28,13 @@ class SettingsAdapter(var settingsList: List<ListItem>) : RecyclerView
     override fun getItemViewType(position: Int): Int {
         //val item = settingsList[position]
         return -1
+    }
+
+    inner class HeaderItemViewHolder(private var binding: ItemRecyclerSettingsTitleBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        fun bind(titleItem: TitleItem) {
+            binding.model = titleItem
+        }
     }
 
 
