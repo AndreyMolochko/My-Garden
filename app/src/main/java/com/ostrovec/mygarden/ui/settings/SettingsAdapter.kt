972c6d64680b8f98a689da7fad96fa10df04e3d4
Andrey Molochko <andrey.molochko@exposit.com>
2019-09-13 11:33:24 +0300

    onBindViewHolder

--- app/src/main/java/com/ostrovec/mygarden/ui/settings/SettingsAdapter.kt
+++ app/src/main/java/com/ostrovec/mygarden/ui/settings/SettingsAdapter.kt
@@ -26,7 +26,20 @@ class SettingsAdapter(var settingsList: List<ListItem>) : RecyclerView
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (settingsList[position]) {
+            is TitleItem -> {
+                val viewHolder = holder as HeaderItemViewHolder
+                viewHolder.bind(settingsList[position] as TitleItem)
+            }
+            is LanguageItem -> {
+                val viewHolder = holder as LanguageItemViewHolder
+                viewHolder.bind(settingsList[position] as LanguageItem)
+            }
+            else -> {
+                val viewHolder = holder as SwitchItemViewHolder
+                viewHolder.bind(settingsList[position] as SwitchItem)
+            }
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -44,16 +57,16 @@ class SettingsAdapter(var settingsList: List<ListItem>) : RecyclerView
         }
     }
 
-    inner class LanguageItemViewHolder(private var binding:ItemRecyclerSettingsLanguageBinding):
-            RecyclerView.ViewHolder(binding.root){
-        fun bind(languageItem:LanguageItem){
+    inner class LanguageItemViewHolder(private var binding: ItemRecyclerSettingsLanguageBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+        fun bind(languageItem: LanguageItem) {
             binding.model = languageItem
         }
     }
 
     inner class SwitchItemViewHolder(private var binding: ItemRecyclerSettingsSwitchBinding)
-        :RecyclerView.ViewHolder(binding.root){
-        fun bing(switchItem: SwitchItem){
+        : RecyclerView.ViewHolder(binding.root) {
+        fun bind(switchItem: SwitchItem) {
             binding.model = switchItem
         }
     }
