74da0916b9d0d91ee4264b42cf8caba242e5eee1
Andrey Molochko <andrey.molochko@exposit.com>
2019-09-14 20:27:55 +0300

    change model of language and switch items

--- app/src/main/java/com/ostrovec/mygarden/ui/settings/model/LanguageItem.kt
+++ app/src/main/java/com/ostrovec/mygarden/ui/settings/model/LanguageItem.kt
@@ -1,6 +1,19 @@
 package com.ostrovec.mygarden.ui.settings.model
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import com.ostrovec.mygarden.BR
+
 class LanguageItem(var id: Int = -1,
-                   var text: String = "") : ListItem {
+                   var text: String = "",
+                   var isVisible: Boolean = false) : ListItem,BaseObservable() {
+
+    var setVisible:Boolean
+    @Bindable
+    get() = isVisible
+    set(value){
+        isVisible = value
+        notifyPropertyChanged(BR.setVisible)
+    }
 
 }
