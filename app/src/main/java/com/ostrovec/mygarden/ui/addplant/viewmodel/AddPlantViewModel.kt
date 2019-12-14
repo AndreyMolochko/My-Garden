7968c4a1d452d1810b30b17b60ad7ce3fc1a218a
AndreyMolochko <Molochko_AG_16@student.grsu.by>
2019-12-14 15:28:10 +0300

    implements method from interface

--- app/src/main/java/com/ostrovec/mygarden/ui/addplant/viewmodel/AddPlantViewModel.kt
+++ app/src/main/java/com/ostrovec/mygarden/ui/addplant/viewmodel/AddPlantViewModel.kt
@@ -7,14 +7,13 @@ import io.reactivex.Completable
 import io.reactivex.Flowable
 import javax.inject.Inject
 
-class AddPlantViewModel @Inject constructor(var plantRepository: PlantRepository) : BaseAddUpdateViewModel
-() {
+class AddPlantViewModel @Inject constructor(var plantRepository: PlantRepository) : BaseAddUpdateViewModel(), AddPlantViewModelType {
 
-    fun addPlant(plant: Plant):Flowable<Long> {
+    override fun addPlant(plant: Plant):Flowable<Long> {
         return plantRepository.insertPlant(plant)
     }
 
-    fun addRemotePlant(plant: Plant):Completable{
+    override fun addRemotePlant(plant: Plant):Completable{
         return plantRepository.addRemotePlant(plant)
     }
 }
